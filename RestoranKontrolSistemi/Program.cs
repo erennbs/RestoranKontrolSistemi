c5ae3c031a1ec1c8f6fd4e24868f352a2e6e17bf
Eren <erenbas3453@gmail.com>
2024-01-30 18:47:48 +0300

    Change Initial Form back to GirisEkrani

--- RestoranKontrolSistemi/Program.cs
+++ RestoranKontrolSistemi/Program.cs
@@ -19,7 +19,7 @@ namespace RestoranKontrolSistemi
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new GirisEkrani());
             
 
         }
